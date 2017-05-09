05e12462576931a59a00315be3601a35c34e7b02
JasonH <jasonh@innov8tive.io>
2017-05-09 13:41:50 -0500

    Migrated and added missing properties on Notification.

--- src/PusherServer/Notification.cs
+++ src/PusherServer/Notification.cs
@@ -5,7 +5,8 @@
         public string title { get; set; }
         public string subtitle { get; set; }
         public string body { get; set; }
-
         public string channel { get; set; }
+        public string senderId { get; set; }
+        public string toId { get; set; }
     }
 }
